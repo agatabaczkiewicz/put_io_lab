4601392914edc57041e011fff81313280725eaf9
unknown <agata.baczkiewicz@gmail.com>
2020-10-29 11:26:31 +0100

    Pierwsza miana

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -9,12 +9,22 @@ public class MyClass {
     System.out.println("Trawa jest zielona");
   }
 
+  
+  public void myPublicMethod2() {
+    System.out.println("gruszka i pietruszka");
+  }
+  
+  
+  //komentarz
+
+
   // Main method
   public static void main(String[] args) {
     myStaticMethod(); // Call the static method
     // myPublicMethod(); This would compile an error
 
-    MyClass myObj = new MyClass(); // Create an object of MyClass
-    myObj.myPublicMethod(); // Call the public method on the object
+    MyClass dwaaaa = new MyClass(); // Create an object of MyClass
+   // Call the public method on the object
+	
   }
 }
